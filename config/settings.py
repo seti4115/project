9283f495243126289a48dd231ee5a1625ad870b8
Setareh <118941362+seti4115@users.noreply.github.com>
2025-07-25 13:47:37 +0330

    user app, user model

--- config/settings.py
+++ config/settings.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-from environs import env
 from pathlib import Path
+
+from environs import env
 
 env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -28,7 +28,6 @@ DEBUG = env('debug')
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # internal
     'home.apps.HomeConfig',
+    'user.apps.UserConfig',
     # external
     'rest_framework',
     'drf_spectacular',
@@ -75,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = env('wsgi_application')
 
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
@@ -85,7 +84,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -105,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
@@ -116,7 +113,6 @@ TIME_ZONE = 'Asia/Tehran'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
@@ -137,3 +133,5 @@ SPECTACULAR_SETTINGS = {
     'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
     'REDOC_DIST': 'SIDECAR',
 }
+
+AUTH_USER_MODEL = 'user.User'
---
+++ user/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.db import models
+
+# Create your models here.
+
+class User(AbstractBaseUser):
+    pass

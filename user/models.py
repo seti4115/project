1dab900573e747f0a1c7d3d13e763080a24c5af6
Setareh <118941362+seti4115@users.noreply.github.com>
2025-08-02 17:18:34 +0330

    edit verbose name field

--- user/models.py
+++ user/models.py
@@ -10,15 +10,14 @@ class UserBase(AbstractUser):
     first_name = models.CharField(_('first name'), max_length=25, validators=[persian_validator, ])
     last_name = models.CharField(_('last name'), max_length=40, validators=[persian_validator, ])
     username = models.CharField(_('username'), max_length=30, unique=True, validators=[english_validator])
-    phone = models.CharField(_("phone number"), max_length=12, validators=[phone_validator], unique=True, db_index=True)
+    phone = models.CharField(_("شماره تلفن"), max_length=12, validators=[phone_validator], unique=True, db_index=True)
     email = models.EmailField(_('email address'), blank=True, null=True)
     activation_code = models.CharField(_('activation code'), max_length=128, editable=False, null=True)
-    is_admin = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False, verbose_name=_('ادمین'))
 
     def save(self, *args, **kwargs):
         if self.activation_code is None:
             self.activation_code = get_random_string(128)
-
         return super(UserBase, self).save(*args, **kwargs)
 
     def __str__(self):

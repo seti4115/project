b74de61e1653f143a9840c0e2cbe1d7707fe0dd0
Setareh <118941362+seti4115@users.noreply.github.com>
2025-08-02 17:20:38 +0330

    permissions for admin panel

--- admin_panel/permissions.py
+++ admin_panel/permissions.py
@@ -1,10 +1,8 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import BasePermission
 
 
 class ISAdminPanelPermission(BasePermission):
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
         return bool(
             request.user and request.user.is_authenticated and request.user.is_admin
         )

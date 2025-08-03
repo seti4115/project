f7f4a8993c19b778564a308f3dd50e0d83731f6a
Setareh <118941362+seti4115@users.noreply.github.com>
2025-08-03 19:14:00 +0330

    edit verbose names & add indexes Meta

--- user/models.py
+++ user/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import Q
 from django.utils.crypto import get_random_string
 from django.utils.translation import gettext as _
 
@@ -7,12 +8,12 @@ from user.validators import phone_validator, persian_validator, english_validato
 
 
 class UserBase(AbstractUser):
-    first_name = models.CharField(_('first name'), max_length=25, validators=[persian_validator, ])
-    last_name = models.CharField(_('last name'), max_length=40, validators=[persian_validator, ])
-    username = models.CharField(_('username'), max_length=30, unique=True, validators=[english_validator])
+    first_name = models.CharField(_('نام'), max_length=25, validators=[persian_validator, ])
+    last_name = models.CharField(_('نام خانوادگی'), max_length=40, validators=[persian_validator, ])
+    username = models.CharField(_('نام کاربری'), max_length=30, unique=True, validators=[english_validator])
     phone = models.CharField(_("شماره تلفن"), max_length=12, validators=[phone_validator], unique=True, db_index=True)
-    email = models.EmailField(_('email address'), blank=True, null=True)
-    activation_code = models.CharField(_('activation code'), max_length=128, editable=False, null=True)
+    email = models.EmailField(_('آدرس ایمیل'), blank=True, null=True)
+    activation_code = models.CharField(_('کد فعالسازی'), max_length=128, editable=False, null=True)
     is_admin = models.BooleanField(default=False, verbose_name=_('ادمین'))
 
     def save(self, *args, **kwargs):
@@ -31,4 +32,11 @@ class UserBase(AbstractUser):
 
 
 class User(UserBase):
-    pass
+    class Meta:
+        verbose_name = _('کاربر')
+        verbose_name_plural = _('کاربران')
+        ordering = ['-phone', '-date_joined']
+        indexes = [
+            models.Index(fields=['phone'], condition=Q(is_active=True), name='phone'),
+            models.Index(fields=['username'], condition=Q(is_active=True), name='username'),
+        ]

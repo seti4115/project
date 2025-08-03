23a2df664995c448267aea27d646a80552b2c189
Setareh <118941362+seti4115@users.noreply.github.com>
2025-08-03 13:02:02 +0330

    edit permission admin panel

--- admin_panel/permissions.py
+++ admin_panel/permissions.py
@@ -1,8 +1,23 @@
 from rest_framework.permissions import BasePermission
 
+from admin_panel.models import Access, AdminPanel
 
-class ISAdminPanelPermission(BasePermission):
+
+class IsAdminPanelPermission(BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            request.user and request.user.is_authenticated and request.user.is_admin
-        )
+        if not request.user.is_authenticated:
+            return False
+        try:
+            return request.user.adminpanel.access in [Access.admin, Access.superuser]
+        except AdminPanel.DoesNotExist:
+            return False
+
+
+class IsSuperAdminPanelPermission(BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        try:
+            return request.user.adminpanel.access == Access.superuser
+        except AdminPanel.DoesNotExist:
+            return False

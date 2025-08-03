3d73b975db8e0f849148365ec9e6b659183cfcc2
Setareh <118941362+seti4115@users.noreply.github.com>
2025-08-03 22:47:56 +0330

    edit api consulting

--- request/serializers.py
+++ request/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-
+from user.validators import phone_validator
 from request.models import Request
 
 
-class UserRequestSerializer(serializers.ModelSerializer):
+class UserRequestConsultingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Request
         fields = ['phone', 'message']
+
+
+    def validate_phone(self, value):
+        phone_validator(value)
+        return value

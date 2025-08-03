14dfded86ec951f8c0db8dc9d4737e9779ecd4cb
Setareh <118941362+seti4115@users.noreply.github.com>
2025-08-03 13:02:58 +0330

    edit api admin panel

--- admin_panel/serializers.py
+++ admin_panel/serializers.py
@@ -7,7 +7,13 @@ User = get_user_model()
 class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'phone']
+
+
+class UserEditSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'is_active', 'is_admin']
 
 
 class AllSerializer(serializers.Serializer):

68d8cdb6bebb162d30b710572993ee0c883ed6d5
Setareh <118941362+seti4115@users.noreply.github.com>
2025-08-04 16:52:00 +0330

    add profile view

--- user/views.py
+++ user/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import login, get_user_model
 from django.utils.translation import gettext as _
-from rest_framework import status
+from rest_framework import status, permissions
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from user.authentications import authenticate
-from user.serializers import UserLoginSerializer, RegisterSerializer
+from user.serializers import UserLoginSerializer, RegisterSerializer, ProfileSerializer
 
 User = get_user_model()
 
@@ -59,3 +60,11 @@ class AuthStatusAPIView(APIView):
         if request.user.is_authenticated:
             return Response({'login': True}, status=status.HTTP_200_OK)
         return Response({"login": False}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserProfileAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request, *args, **kwargs):
+        user = get_object_or_404(User, pk=request.user.id)
+        serializer = ProfileSerializer(user)
+        return Response(serializer.data)

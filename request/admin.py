48fa61507d7c354f1fd298d2d3c30d17df9136ff
Setareh <118941362+seti4115@users.noreply.github.com>
2025-10-17 17:16:45 +0330

    register spraying model

--- request/admin.py
+++ request/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import ConsultingRequest
+from .models import ConsultingRequest, SprayingRequest
 
 
 @admin.register(ConsultingRequest)
-class RequestAdmin(admin.ModelAdmin):
+class ConsultingRequestAdmin(admin.ModelAdmin):
+    list_display = ['phone', 'type', 'status', 'created_at']
+
+@admin.register(SprayingRequest)
+class SprayingRequestAdmin(admin.ModelAdmin):
     list_display = ['phone', 'type', 'status', 'created_at']

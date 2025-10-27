06753bf001823761cadb6bdf619b74a19b17ba92
Setareh <118941362+seti4115@users.noreply.github.com>
2025-10-27 14:30:21 +0330

    django admin: jalali date

--- request/admin.py
+++ request/admin.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
+from persiantools.jdatetime import JalaliDateTime
 
 from .models import ConsultingRequest, SprayingRequest
 
 
 @admin.register(ConsultingRequest)
 class ConsultingRequestAdmin(admin.ModelAdmin):
-    list_display = ['phone', 'type', 'status', 'created_at']
+    list_display = ['phone', 'status', 'created_at_to_jalali']
+    list_filter = ['status', 'created_at']
+    list_editable = ['status']
+
+    def created_at_to_jalali(self, obj):
+        jdt = JalaliDateTime.to_jalali(obj.created_at)
+        return jdt.strftime("%H:%M  %Y/%m/%d")
+
+    created_at_to_jalali.short_description = "ایجاد شده در"
+
 
 @admin.register(SprayingRequest)
 class SprayingRequestAdmin(admin.ModelAdmin):
-    list_display = ['phone', 'type', 'status', 'created_at']
+    list_display = ['phone', 'type', 'status', 'created_at_to_jalali']
+    list_filter = ['status', 'created_at']
+    list_editable = ['status']
+
+    def created_at_to_jalali(self, obj):
+        jdt = JalaliDateTime.to_jalali(obj.created_at)
+        return jdt.strftime("%H:%M  %Y/%m/%d")
+
+    created_at_to_jalali.short_description = "ایجاد شده در"

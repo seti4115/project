baf93d85670b9b7ac4b3878d66c212d80c66450a
Setareh <118941362+seti4115@users.noreply.github.com>
2025-10-27 14:32:40 +0330

    def date to jalali

--- utils/methods.py
+++ utils/methods.py
@@ -1,3 +1,5 @@
+from persiantools.jdatetime import JalaliDate
+
 
 def ip_address(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -6,3 +8,8 @@ def ip_address(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
+
+def jalali_to_gregorian(date_str):
+    y, m, d = map(int, date_str.replace("/", "-").split("-"))
+    jdate = JalaliDate(y, m, d)
+    return jdate.to_gregorian()

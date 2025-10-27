786f00e9e35ca616ef76ee9c12e3efc3f71be3f7
Setareh <118941362+seti4115@users.noreply.github.com>
2025-10-27 22:34:11 +0330

    product api

---
+++ product/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from taggit.serializers import TaggitSerializer, TagListSerializerField
+
+from product.models import Product
+
+
+class ProductAllFieldSerializer(TaggitSerializer, ModelSerializer):
+    tags = TagListSerializerField()
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductShowSerializer(TaggitSerializer, ModelSerializer):
+    link = serializers.SerializerMethodField()
+    tags = TagListSerializerField()
+
+    class Meta:
+        model = Product
+        fields = ['title', 'link', 'weight', 'type', 'brand', 'description', 'price', 'production_date',
+                  'expiration_date', 'tags']
+
+    def get_link(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.get_absolute_url())

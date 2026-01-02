bc11a8a47d61f2926093d1e4819bfd64ad933e51
Setareh <118941362+seti4115@users.noreply.github.com>
2026-01-03 00:22:07 +0330

    change auto add & delete admin panel with is_admin

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -17,16 +17,6 @@ class AdminPanel(models.Model):
     access = models.CharField(choices=Access.choices, default=Access.admin, max_length=15, verbose_name=_('access'))
 
 
-    def delete(self, **kwargs):
-        self.user.is_admin = False
-        self.user.save()
-        return super().delete(**kwargs)
-    
-    def save(self, **kwargs):
-        self.user.is_admin = True
-        self.user.save()
-        return super().save(**kwargs)
-        
     
     def __str__(self):
         return f"{self.user}"
--- user/admin.py
+++ user/admin.py
@@ -17,13 +17,13 @@ class UserAdmin(admin.ModelAdmin):
     list_editable = ['is_active', 'is_staff', 'is_superuser']
     ordering = ['-id', 'is_active']
     inlines = [AdminPanelInline]
-
-    def save_model(self, request, obj, form, change):
-
-        if obj.is_admin and not hasattr(obj, 'adminpanel'):
-            AdminPanel.objects.create(user=obj)
-
-        if not obj.is_admin and hasattr(obj, 'adminpanel'):
-            AdminPanel.objects.get(user=obj).delete()
-
-        return super(UserAdmin, self).save_model(request, obj, form, change)
+    #
+    # def save_model(self, request, obj, form, change):
+    #
+    #     if obj.is_admin and not hasattr(obj, 'adminpanel'):
+    #         AdminPanel.objects.create(user=obj)
+    #
+    #     if not obj.is_admin and hasattr(obj, 'adminpanel'):
+    #         AdminPanel.objects.get(user=obj).delete()
+    #
+    #     return super(UserAdmin, self).save_model(request, obj, form, change)
